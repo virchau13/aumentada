e1c53da10f05283cd5995547eeac1eb35e73b08c
virchau13 <virchau13@hexular.net>
2022-02-15 21:21:55 +0800

    fix(nix): get nix build working

--- flake.nix
+++ flake.nix
@@ -2,12 +2,17 @@
     description = "A Nix flake deployment tool";
 
     inputs = {
+        naersk.url = github:nix-community/naersk;
         utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, utils  }: utils.lib.eachDefaultSystem (system: 
+    outputs = { self, nixpkgs, utils, naersk }: utils.lib.eachDefaultSystem (system: 
         let 
-            pkgs = nixpkgs.legacyPackages."${system}";
+            pkgs = nixpkgs.legacyPackages.${system};
+            aumentada-rust = naersk.lib.${system}.buildPackage {
+                root = ./.;
+                copyLibs = true;
+            };
             aumentada = pkgs.mkYarnPackage {
                 pname = "aumentada";
                 version = "0.1.0";
@@ -22,19 +27,26 @@
                 ];
                 buildPhase = ''
                     pushd deps/aumentada/
+                    mkdir -p dist/
                     echo 'ls -al'
                     ls -al
-                    yarn build
+                    cp ${aumentada-rust}/lib/libaumentada.so dist/index.node
+                    yarn --offline build-js
                     popd
                 '';
                 installPhase = ''
+                    pushd deps/aumentada
                     mkdir -p $out/bin
                     mv dist/ $out/dist
+                    cp -r ../../node_modules $out/dist/
                     cat >$out/bin/aumentada <<EOF
 #!${pkgs.bash}/bin/bash
-${pkgs.nodejs}/bin/node $out/dist/index.js
+PATH="${pkgs.fortune}/bin:\$PATH" ${pkgs.nodejs}/bin/node $out/dist/index.js
 EOF
+                    popd
+                    chmod +x $out/bin/aumentada
                 '';
+                distPhase = "true";
             };
         in {
             defaultPackage = aumentada;
